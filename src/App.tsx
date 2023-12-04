79f5d8e5d0e1ffc3cb8b06a2a2b9f87ec8b0b478
EaudeJavel <p.plamy6@gmail.com>
2023-12-04 16:33:33 +0100

    ui: HeaderMenuRightLogoLeft + basic icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Headers from "./pages/Headers";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="headers" element={<h1>Headers</h1>} />
+        <Route index path="/" element={<h1>Home</h1>} />
+        <Route path="headers" element={<Headers />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Headers.tsx
@@ -0,0 +1,12 @@
+import HeaderMenuRightLogoLeft from "../ui/headers/HeaderMenuRightLogoLeft";
+
+const Headers = () => {
+  return (
+    <>
+      <h1 className=" text-4xl">Headers</h1>
+      <HeaderMenuRightLogoLeft />
+    </>
+  );
+};
+
+export default Headers;
