dd5f55bd29fe1cd20179fe7cb088ddb39dd0e709
EaudeJavel <p.plamy6@gmail.com>
2023-12-09 19:56:06 +0100

    fix: minor slider fixes

--- src/ui/elements/Slider.tsx
+++ src/ui/elements/Slider.tsx
@@ -1,3 +1,5 @@
+import Card from "./Card";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Scrollbar } from "swiper/modules";
@@ -12,96 +14,26 @@ const Slider = () => {
       scrollbar={{
         hide: true,
       }}
-      slidesPerView={3}
-      spaceBetween={30}
+      slidesPerView={"auto"}
+      spaceBetween={10}
       loop={true}
       modules={[Scrollbar]}
       className="mySwiper"
     >
-      <SwiperSlide>
-        <div className="card w-full bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      <SwiperSlide className="w-96 h-60 flex self-center">
+        <Card />
       </SwiperSlide>
-      <SwiperSlide>
-        <div className="card w-full bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      <SwiperSlide className="w-96 h-60 flex self-center">
+        <Card />
       </SwiperSlide>
-      <SwiperSlide>
-        <div className="card w-full bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      <SwiperSlide className="w-96 h-60 flex self-center">
+        <Card />
       </SwiperSlide>
-      <SwiperSlide>
-        <div className="card w-full bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      <SwiperSlide className="w-96 h-60 flex self-center">
+        <Card />
       </SwiperSlide>
-      <SwiperSlide>
-        <div className="card w-full bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      <SwiperSlide className="w-96 h-60 flex self-center">
+        <Card />
       </SwiperSlide>
     </Swiper>
   );
