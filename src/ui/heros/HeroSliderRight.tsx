2ff15fdc02fe90d7f6e8c7d6b8e56cb49cf76654
EaudeJavel <p.plamy6@gmail.com>
2023-12-10 12:07:08 +0100

    fix: HeroSliderRight comments

--- src/ui/heros/HeroSliderRight.tsx
+++ src/ui/heros/HeroSliderRight.tsx
@@ -4,10 +4,10 @@ import Slider from "../elements/Slider";
 
 const HeroSliderRight = () => {
   return (
-    // grid w-full grid-cols-1 lg:max-w-xl lg:mb-0 lg:mt-0 lg:py-8 lg:ml-auto
     <>
       {/* min height 500px */}
       <div className="relative w-full flex flex-col justify-center bg-base-200 min-h-[500px] lg:flex-row ">
+        {/* lg:max-w-[40rem] is the half of max-w-7xl */}
         <div className="flex my-10 px-4 sm:px-16 lg:w-1/2 lg:m-auto lg:mr-0 lg:max-w-[40rem] 2xl:grid-cols-2 2xl:w-full 2xl:max-w-7xl">
           <div className="flex flex-col items-center justify-center w-full lg:items-start ">
             <h1 className="text-5xl text-center font-semibold lg:text-left">
@@ -20,12 +20,11 @@ const HeroSliderRight = () => {
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
-
-          {/* <img src={heroImg} className="w-2/5 hidden md:block" /> */}
         </div>
 
         {/* slider */}
-        <div className="w-full top-0 right-0 left-0 bottom-0 lg:w-1/2 2xl:left-auto 2xl:absolute ">
+        {/* w-1/2 when relative for half size with title */}
+        <div className="w-full  lg:w-1/2 2xl:top-0 2xl:right-0 2xl:bottom-0 2xl:left-auto 2xl:absolute ">
           <div className="right-0 top-0 bottom-0 px-12 flex justify-center lg:w-1/2 2xl:right-0 lg:max-w-7xl lg:absolute">
             <Slider />
           </div>
